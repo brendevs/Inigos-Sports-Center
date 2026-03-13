f1345ddea7925287c38a0528b76e5200390b4fff
jan brendell amandy <fbrendellcali@gmail.com>
2026-03-14 01:41:38 +0800

    (inigo-5):date picker

--- app/court/[id]/page.tsx
+++ app/court/[id]/page.tsx
@@ -1,20 +1,16 @@
-interface Props {
-  params: {
-    id: string;
-  };
-}
+import { DatePickerDemo } from "../../components/Shadcn/datePicker";
 
 export default function Schedule() {
   return (
-    <div className="min-h-screen p-6 bg-gray-50 flex flex-col gap-6 bg-white ">
+    <div className="min-h-screen p-6 bg-gray-50 flex flex-col gap-6 bg-white font-sans">
       <h1 className="text-2xl font-bold font-sans">Schedule</h1>
       <div>
         <div>
-          <p>date</p>
+          <DatePickerDemo /> {/* ✅ Capitalized */}
           <p>time</p>
         </div>
         <div>
-          <p> show available court</p>
+          <p className="font-sans">Show available court</p>
         </div>
       </div>
       <p className="font-bold"></p>
